c0ae7a533883ece71127d80669d70dd8948b8882
laidouni <mohamed.laidouni@etu.u-paris.fr>
2025-09-10 15:47:11 +0200

    [fix] Prevent API Error
    -> modified
        frontend/src/api : location.tsx
        frontend/src/api : weather.tsx

--- frontend/src/api/location.tsx
+++ frontend/src/api/location.tsx
@@ -3,5 +3,6 @@ export async function fetchLocationSuggestions(query: string) {
   const res = await fetch(
     `/api/location/search?query=${encodeURIComponent(query)}`
   );
+  if (!res.ok) throw new Error(`Erreur API: ${res.status}`);
   return res.json();
 }
--- frontend/src/api/weather.tsx
+++ frontend/src/api/weather.tsx
@@ -3,5 +3,6 @@ export async function fetchMeteoFromLocation(lat: number, lon : number) {
   const res = await fetch(
     `/api/weather/current?latitude=${(lat)}&longitude=${(lon)}`
   );
+  if (!res.ok) throw new Error(`Erreur API: ${res.status}`);
   return res.json();
 }
