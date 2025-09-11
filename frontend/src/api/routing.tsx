c334d4560bebe6a26cc8247ae0bd963615ffa718
laidouni <mohamed.laidouni@etu.u-paris.fr>
2025-09-11 16:01:16 +0200

    [fix] fetchRoute with rain condition
    -> modified
        frontend/src/api : routing.tsx

--- frontend/src/api/routing.tsx
+++ frontend/src/api/routing.tsx
@@ -1,5 +1,5 @@
 export async function fetchRoutingWeatherAware(startLatLng: L.LatLng, endLatLng: L.LatLng) {
-	const url = `/api/routing/weather-aware?startLat=${startLatLng.lat}&startLng=${startLatLng.lng}&endLat=${endLatLng.lat}&endLng=${endLatLng.lng}`;
+	const url = `/api/routing/weather-aware?startLat=${startLatLng.lat}&startLng=${startLatLng.lng}&endLat=${endLatLng.lat}&endLng=${endLatLng.lng}&avoidConditions=rain`;
 	const res = await fetch(url);
 	if (!res.ok) throw new Error(`Erreur API: ${res.status}`);
 	return res.json();
