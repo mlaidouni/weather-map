fdc48ee9592b1bb87fab2a22d916d2c9984dd133
friedman <eliot.friedmann@etu.u_paris.fr>
2025-09-12 12:23:49 +0200

    pin jolie

--- frontend/src/components/card/MapCard.tsx
+++ frontend/src/components/card/MapCard.tsx
@@ -20,24 +20,29 @@ import { AreaPrevisionRoute } from "@/types/areaPrevisionRoute";
 const apikey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 const DEBUG = import.meta.env.DEBUG === 'true';
 
-
+// Icône de départ (flèche verte)
+// Icône de départ (verte)
 const startIcon = L.icon({
-	iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-	shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41],
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
+// Icône d’arrivée (rouge)
 const endIcon = L.icon({
-	iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-	shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-	iconSize: [25, 41],
-	iconAnchor: [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize: [41, 41],
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
+
+
+
 
 L.Marker.prototype.options.icon = startIcon;
 
